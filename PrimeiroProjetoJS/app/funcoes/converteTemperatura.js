9b91e730fad2eeb8e749e375a7551e91cb44faae
karen <81941451+kahbeltcher@users.noreply.github.com>
2024-01-12 11:55:31 -0300

    Delete PrimeiroProjetoJS directory

--- PrimeiroProjetoJS/app/funcoes/converteTemperatura.js
+++
@@ -1,9 +0,0 @@
-function converteCelsiusParaFahrenheit(celsius) {
-  let fahrenheit;
-
-  fahrenheit = celsius * 1.8 + 32;
-
-  return fahrenheit;
-}
-
-export default converteCelsiusParaFahrenheit;
--- PrimeiroProjetoJS/app/senha.js
+++
@@ -1,9 +0,0 @@
-import valida_senha from "./funcoes/validaSenha.js";
-
-let senha = "minhasenha";
-
-if (valida_senha(senha)) {
-  console.log("A senha possui no mínimo 10 caracteres");
-} else {
-  console.log("A senha não possui o mínimo 10 caracteres");
-}
